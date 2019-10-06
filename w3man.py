586145831010147ff9168a05ba794ddbbf7c05d0
Shashank Babu <shashank.sj1@programmer.net>
2019-10-06 21:18:12 +0530

    reduced file size

--- w3man.py
+++ w3man.py
@@ -17,11 +17,13 @@ a='''
                                                           
 '''
 print(a)
+
 #tag=sys.argv[1]
 #url="https://www.w3schools.com/tags/tag_"+tag+".asp"
 t=input()
 url="https://www.w3schools.com/tags/tag_"+t+".asp"
 b= urllib.request.urlopen(url).read()
+
 #soup = BeautifulSoup(b)
 soup = BeautifulSoup(b, 'html.parser')
 
@@ -34,15 +36,14 @@ text = soup.get_text()
 
 # break into lines and remove leading and trailing space on each
 lines = (line.strip() for line in text.splitlines())
+
 # break multi-headlines into a line each
 chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
 
-#print(text)
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)     #print(text)
 flag=0
-#global count
-count=0
+count=0         #global count
 s=""
 c=0
 for i in text.split('\n'):
@@ -58,8 +59,7 @@ for i in text.split('\n'):
         print("EXAMPLE\n")
         continue
     elif(i=="Try it Yourself »"):
-        flag=0
-        
+        flag=0    
     if(flag==1):
         print(i)
     if((flag==2)and(count!=0)):
@@ -73,7 +73,3 @@ s=' . '.join(x for x in r)
 s=s[::-1]
 s=s+'.'
 print(s)
- 
-
-
-
